58b3e10c881c9d2a2fa580c740eb381bdaabc353
kesava <keshanna78160@gmail.com>
2023-10-15 19:04:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Card from './components/Card'
+import React, { Component } from 'react';
+import styles from './styles/Main.module.css';
+import loaderimg from './Loader/preloader.gif';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      api : [],
+      loader: true
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://www.omdbapi.com/?apikey=45f0782a&s=war")
+    .then(response => response.json())
+    .then(json => setTimeout(()=> this.setState({api : json, loader:false}),2000))
+  }
+
+  render(){ 
+    let cardArr = this.state.api.Search
+    console.log(this.state.loader)
+    return ( 
+    <>
+      <div className={ styles.Main }>
+        { this.state.loader ? <img src={ loaderimg } alt=""/> : 
+          <div className={ styles.CardGrid }>
+            { cardArr ?  cardArr.map(item => <Card props={item}/>)   : null }
+          </div>
+        }
+      </div>
+    </>
+    );
+  }
 }
+ 
 
 export default App;
